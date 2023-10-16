037563dc53df750d821930a4b7fa48c3ebe4321a
AnaMTF <anamaria.titeche@gmail.com>
2023-10-16 14:36:53 +0300

    queryselectors and project

--- Udemy/16.1 DOM Challenge Starting Files/index.js
+++ Udemy/16.1 DOM Challenge Starting Files/index.js
@@ -1,1 +1,18 @@
-document.query
+document.query;
+document.getElementsByTagName("li")[2].innerHTML = "Ana";
+
+document.getElementsByTagName("li")[1].style.color = "purple";
+document.getElementById("title").innerHTML = "Good bye";
+document.querySelector("#title").style.color = "green";
+document.querySelector("li a").style.color = "red";
+document.querySelector("button").style.backgroundColor = "yellow";
+document.querySelectorAll("#list .item")[2].style.color = "blue"; //not working
+
+document.querySelector("button").classList;
+document.querySelector("button").classList.add("invisible");
+document.querySelector("button").classList.remove("invisible");
+document.querySelector("button").classList.toggle("invisible");
+
+document.querySelector("a").attributes;
+document.querySelector("a").getAttribute("href");
+document.querySelector("a").setAttribute("href", "www.bing.com");
