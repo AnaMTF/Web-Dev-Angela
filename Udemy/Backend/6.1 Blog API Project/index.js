b3f799d0b39a6fc66afb426738bf1f2967eb486b
AnaMTF <anamaria.titeche@gmail.com>
2023-11-02 16:17:00 +0200

    complete blog API with all commands + fix bug that wasnt allowing content to be updated

--- Udemy/Backend/6.1 Blog API Project/index.js
+++ Udemy/Backend/6.1 Blog API Project/index.js
@@ -41,14 +41,69 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Write your code here//
 
 //CHALLENGE 1: GET All posts
+app.get("/posts", (req, res)=>{
+  console.log(posts);
+  res.json(posts);
 
+});
 //CHALLENGE 2: GET a specific post by id
-
+app.get("/posts/:id", (req, res)=>{
+  const postId = parseInt(req.params.id);
+  const post = posts.find((p) => p.id === postId);
+  if(!post){
+    return res.status(404).json({message: `The post with ID ${postId} was not found`});
+  }
+  else{
+    res.json(post);
+  }
+})
 //CHALLENGE 3: POST a new post
-
+app.post("/posts", (req, res)=>{
+  const newId = lastId +=1;
+  const newPost = {
+    id: newId,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    date: new Date(),
+  };
+  lastId = newId;
+  posts.push(newPost);
+  res.status(201).json(newPost);
+})
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
+app.patch("/posts/:id", (req, res)=>{
+  const postId = parseInt(req.params.id);
+  const post = posts.find((p)=> p.id === postId);
+  if(!post){
+    return res.status(404).json({message: `The post with ID ${postId} was not found`});
+  }
+  if(req.body.title){
+    post.title = req.body.title;
+    console.log(post.title);
+  } 
+  if(req.body.content) {
+    post.content = req.body.content;
+    console.log(post.content);
+  }
+  if(req.body.author) {
+    post.author = req.body.author;
+    console.log(post.author);
+  }
 
+  res.json(post);
+});
 //CHALLENGE 5: DELETE a specific post by providing the post id.
+app.delete("/posts/:id", (req, res)=>{
+  const postId = parseInt(req.params.id);
+  const postIndex = posts.findIndex((p)=> p.id === postId);
+  if(postIndex === -1){
+    return res.status(404).json({message: `The post with ID ${postId} was not found`});
+  }
+  posts.splice(postIndex, 1);
+  res.json({message: `The post with ID ${postId} was deleted`});
+})
+
 
 app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}`);
