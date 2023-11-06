67a8c33ffcf0fe99cc39675c34fd9af546d13422
AnaMTF <anamaria.titeche@gmail.com>
2023-11-06 23:55:45 +0200

    complete query to add new countries

--- Udemy/Backend/8.3 Travel Tracker/index.js
+++ Udemy/Backend/8.3 Travel Tracker/index.js
@@ -18,6 +18,7 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// get home page
 app.get("/", async (req, res) => {
   //Write your code here.
   const result = await db.query("SELECT country_code FROM visited_countries");
@@ -27,7 +28,27 @@ app.get("/", async (req, res) => {
   });
   console.log(result.rows);
   res.render("index.ejs", { countries: countries, total: countries.length });
-  db.end();
+});
+
+//insert a new country
+app.post("/add", async (req, res) => {
+  //Write your code here.
+  const input = req.body["country"];
+
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1",
+    [input]
+  );
+
+  if (result.rows.length !== 0) {
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      countryCode,
+    ]);
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
