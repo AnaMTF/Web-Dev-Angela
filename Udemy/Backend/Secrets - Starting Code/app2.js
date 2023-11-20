5e066bebc99af1cfbd5b8e2cbffe4fd30175d04a
AnaMTF <anamaria.titeche@gmail.com>
2023-11-20 18:35:56 +0200

    complete app2 with level 2 encryption

---
+++ Udemy/Backend/Secrets - Starting Code/app2.js
@@ -0,0 +1,68 @@
+import express from "express";
+import bodyParser from "body-parser";
+import ejs from "ejs";
+import pg from "pg";
+
+const secretKey = "secretkey"; 
+const app = express();
+const port = 3000;
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "userdb",
+  password: "papusica123",
+  port: 5432,
+});
+db.connect();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+let users = [
+  { id: 1, email: "ana_titeche@yahoo.com", password: "test123" },
+];
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.post("/login", async (req, res) => {
+    const email = req.body.username;
+    const password = req.body.password;
+    try {
+        const result = await db.query("SELECT username, pgp_sym_decrypt(password, ($1)) AS password FROM users2 WHERE username=($2)", [secretKey, email]);
+        const user = result.rows[0];
+        console.log(user);
+        if (user.password == password) {
+            res.render("secrets.ejs");
+        };
+    } catch (err) {
+        console.log(err.message);
+    };
+})
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+    const email = req.body.username;
+    const password = req.body.password;
+    try {
+        await db.query("INSERT INTO users2 (username, password) VALUES ($1, pgp_sym_encrypt(($2), ($3)))", [email, password, secretKey]);
+        res.render("secrets.ejs");
+    } catch (err) {
+        console.log(err.message);
+    };
+    
+})
+
+
+app.listen(3000, function() {
+    console.log(`Server started on port ${port}`);
+});
