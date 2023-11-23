b81edf30891484c949c251b9c23e32c7506b2431
AnaMTF <anamaria.titeche@gmail.com>
2023-11-24 00:34:33 +0200

    add dotenv file

--- Udemy/Backend/Secrets - Starting Code/app3.js
+++ Udemy/Backend/Secrets - Starting Code/app3.js
@@ -1,1 +1,11 @@
-require ('dotenv').config();
+import dotenv from "dotenv"; dotenv.config();
+
+import express from "express";
+import bodyParser from "body-parser";
+import ejs from "ejs";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+console.log(process.env)
